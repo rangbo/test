e808f4caf957dc2707dcef00f9e78331df6157d9
rangbo <partypeeoplle@naver.com>
2022-03-27 20:41:41 +0900

    22.03.27 헤더수정

--- js/script.js
+++ js/script.js
@@ -20,3 +20,16 @@ $(document).ready(function(){
         $(".drop_menu").stop().slideToggle();
     });
 });
+
+$(function() {
+    var lnb = $("#lnb").offset().top;
+    $(window).scroll(function() {
+        var window = $(this).scrollTop();
+
+        if(lnb <= window) {
+            $("#lnb").addClass("fixed");
+        } else {
+            $("#lnb").removeClass("fixed");
+        }
+    })
+});
