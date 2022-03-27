329d46449f6591b906c65da9ddf5e7c2d40d5ddb
rangbo <partypeeoplle@naver.com>
2022-03-27 15:53:30 +0900

    22.03.27 드롭다운메뉴수정

--- js/script.js
+++ js/script.js
@@ -11,5 +11,12 @@
 
 });
 
-
 })(jQuery);
+
+$(document).ready(function(){
+    $(".drop_menu").hide();
+    
+    $("header .menu>li, .drop_menu").hover(function(){
+        $(".drop_menu").stop().slideToggle();
+    });
+});
